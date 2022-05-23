aa23d40ae99f136d55e91e14515fb00db00db37b
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-23 10:13:18 -0400

    imported Shop.js to RouteSwitch, added shop path to Routes in RouteSwitch component

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-
+import Shop from "./Shop";
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App></App>}></Route>
+                <Route path="/shop" element={<Shop></Shop>}></Route>
             </Routes>
         </BrowserRouter>
     )
