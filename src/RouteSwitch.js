85e3f4aa43de9ca5ca4a394a9c2e27746667c9c7
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-23 09:47:03 -0400

    added a rurn statement thats rendering the BrowserRouter and the Routes component

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,6 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 const RouteSwitch = () => {
-
+    return (
+        <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+        </BrowserRouter>
+    )
 }
 export default RouteSwitch;
