e922d647470e96ba044383a48d37c25789202fbc
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-23 10:11:57 -0400

    added Shop Component

---
+++ src/Shop.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+function Shop () {
+    return <div> 
+        <NavBar></NavBar>
+        <h1> Welcome to the shop!</h1>
+    </div>
+}
+export default Shop;
