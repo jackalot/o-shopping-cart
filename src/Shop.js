e9669b3da48fb399d38096af2c7f77b16def7f15
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-23 10:14:50 -0400

    added the () to the return statement in the shop component

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 function Shop () {
-    return <div> 
+    return (
+    <div> 
         <NavBar></NavBar>
         <h1> Welcome to the shop!</h1>
     </div>
+    );
 }
 export default Shop;
