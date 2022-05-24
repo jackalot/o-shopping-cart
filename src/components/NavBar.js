5e2ac01eaff6550cf695b7f39565101bf9cad5a8
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-24 08:50:56 -0400

    added two classes to NavBar elements 'NavBar-Container' and 'NavBar-UL'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar () {
- return <div> 
-     <ul>
+ return <div className="NavBar-Container"> 
+     <ul className="NavBar-Ul">
         <li><Link to={"/"}>Home</Link></li>
         <li><Link to={"/shop"}>shop</Link></li>
      </ul>
