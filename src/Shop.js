0254bf6ccdfffbc59e8f3cf9367ecf92b7da3325
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-24 08:45:19 -0400

    imported ShopItem to App.js and added a sample ShopItem to be rendered

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CartNotification from "./components/CartNotification";
+import ShopItem from "./components/ShopItem";
 function Shop () {
     return (
     <div> 
         <h1> Welcome to the shop!</h1>
         <CartNotification amount={1}></CartNotification>
+        <ShopItem itemName="Taco" itemPrice="10.00"></ShopItem>
     </div>
     );
 }
