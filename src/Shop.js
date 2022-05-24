98f0384d16cc5d6b36218d990380c41dc8383840
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-24 08:37:06 -0400

    added a import and rendered an instance of CartNotification in ShopJs

--- src/Shop.js
+++ src/Shop.js
@@ -1,8 +1,10 @@
 import React from "react";
+import CartNotification from "./components/CartNotification";
 function Shop () {
     return (
     <div> 
         <h1> Welcome to the shop!</h1>
+        <CartNotification amount={1}></CartNotification>
     </div>
     );
 }
