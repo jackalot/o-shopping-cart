f5671d2412c07330280f6aaff486766faa5670dc
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-24 08:00:03 -0400

    imported some packes to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import "@testing-library/jest-dom"
+import userEvent from '@testing-library/user-event';
+import TestComponent from "path-to-test-component";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
