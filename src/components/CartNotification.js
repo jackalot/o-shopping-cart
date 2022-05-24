ec76834e18cba60fd9d939cc3ae65708617bf311
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-24 08:17:35 -0400

    created a CartNotification component inside CartNotification

---
+++ src/components/CartNotification.js
@@ -0,0 +1,9 @@
+import React from "react";
+function CartNotification (props) {
+    return <div>
+        <div>
+            {props.amount}
+        </div>
+    </div>
+}
+export default CartNotification;
