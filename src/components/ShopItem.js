43004e3947d24218c480fb7e136ed26c11dbb7d9
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 07:53:10 -0400

    wrapped the itemName in a div

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -2,7 +2,9 @@ import React from "react";
 import '../css-component-files/ShopItem.css'
 function ShopItem (props) {
     return <div className="ShopItem-Container">
+        <div className="ShopItem-ItemName">
         {props.itemName}
+        </div>
         <div className="ShopItem-Price">
         {props.itemPrice}
         </div>
