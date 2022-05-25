916133fb7667c56a336aea9638fc6d2f53b9c425
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 08:44:32 -0400

    imported Cart.js to Shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CartNotification from "./components/CartNotification";
 import ShopItem from "./components/ShopItem";
+import Cart from "./components/Cart";
 function Shop () {
     return (
     <div> 
         <h1> Welcome to the shop!</h1>
         <CartNotification amount={1}></CartNotification>
+        <Cart></Cart>
         <ShopItem itemName="Taco" itemPrice="10.00"></ShopItem>
     </div>
     );
