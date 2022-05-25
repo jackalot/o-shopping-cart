1f22849ddd7a80cb91f1c2b442b1293a956f7ae0
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 09:31:08 -0400

    added some elements so we can get a general layout for how the Cart will look when we do get Carts selected

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,12 @@ import "../css-component-files/Cart.css"
 function Cart (props) {
     
     return <div className="Cart-container">
-        
+        <ul>
+            <li>Taco, Price Per: 10.00, Quantity Selected: 5 Total: $50.00</li>
+            <li>Nachos, Price Per: 5.00, Quantity Selected: 10 Total: $50.00</li>
+            <li>Burrito, Price Per: 3.00, Quantity Selected: 2 Total: $6.00</li>
+        </ul>
+        <p>TOTAL PRICE: $106.00</p>
     </div>
 }
 export default Cart
