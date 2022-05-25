cf558eed0d0055875a8a8de0f13dbca023c6b82a
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 08:10:01 -0400

    it now says Price next to the itemPrice

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -3,10 +3,10 @@ import '../css-component-files/ShopItem.css'
 function ShopItem (props) {
     return <div className="ShopItem-Container">
         <div className="ShopItem-ItemName">
-        {props.itemName}
+       {props.itemName}
         </div>
         <div className="ShopItem-Price">
-        {props.itemPrice}
+        Price: {props.itemPrice}
         </div>
         <label for={props.itemName + "quantity"}>Quantity</label>
         <input className="QuantityInput" name={props.itemName + "quantity"} type="number"></input>
