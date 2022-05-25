b28aeab80c282559355085fca6636b40b8acb8bc
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 09:03:03 -0400

    removed items from both shop and cart.js

--- src/Shop.js
+++ src/Shop.js
@@ -7,25 +7,7 @@ function Shop () {
     <div> 
         <h1> Welcome to the shop!</h1>
         <CartNotification amount={1}></CartNotification>
-        <Cart items={[{
-            Taco: {
-               pricePer: 10.00,
-                Quantity: 3, 
-            }
-        },
-        {
-            Nacho: {
-               pricePer: 5.00,
-                Quantity: 4, 
-            }
-        },
-        {
-            Burrito: {
-               pricePer: 2.50,
-                Quantity: 6, 
-            }
-        }
-        ]}></Cart>
+        <Cart></Cart>
         <ShopItem itemName="Taco" itemPrice="10.00"></ShopItem>
     </div>
     );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../css-component-files/Cart.css"
 function Cart (props) {
+    
     return <div className="Cart-container">
-
+        
     </div>
 }
 export default Cart
