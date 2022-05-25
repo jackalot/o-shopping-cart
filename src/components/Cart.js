67246b868da7c7bb69395a1dc27e8885dfbd2ede
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-25 09:36:03 -0400

    added classes to the li's in Cart.js, Cart.css now applys to them instead of li's in general

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,9 +4,9 @@ function Cart (props) {
     
     return <div className="Cart-container">
         <ul>
-            <li>Taco, Price Per: 10.00, Quantity Selected: 5 Total: $50.00</li>
-            <li>Nachos, Price Per: 5.00, Quantity Selected: 10 Total: $50.00</li>
-            <li>Burrito, Price Per: 3.00, Quantity Selected: 2 Total: $6.00</li>
+            <li className="Cart-cartItem">Taco, Price Per: 10.00, Quantity Selected: 5 Total: $50.00</li>
+            <li className="Cart-cartItem">Nachos, Price Per: 5.00, Quantity Selected: 10 Total: $50.00</li>
+            <li className="Cart-cartItem">Burrito, Price Per: 3.00, Quantity Selected: 2 Total: $6.00</li>
         </ul>
         <p>TOTAL PRICE: $106.00</p>
     </div>
