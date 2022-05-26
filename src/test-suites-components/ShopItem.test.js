3ec0d8563e930563a2b8f12bd8e08fe6e532d681
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-26 08:12:06 -0400

    created a test for the SHopItem component that was supposed to check if a div had the Price text in it

--- src/test-suites-components/ShopItem.test.js
+++ src/test-suites-components/ShopItem.test.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { ... } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import ShopItem from "../components/ShopItem";
+
+describe("ShopItem component", () => {
+    it("renders correct div", ()=> {
+        const { getByRole } = render(<ShopItem></ShopItem>);
+        expect(getByRole("div").textContent).toMatch("Price");
+    })
+})
