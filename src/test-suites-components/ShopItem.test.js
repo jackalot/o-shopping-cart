21745d8dd1c5881a78a06ff4f7e14c0c7dea8b6c
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-26 08:39:52 -0400

    removed the second test

--- src/test-suites-components/ShopItem.test.js
+++ src/test-suites-components/ShopItem.test.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
-import { render } from "@testing-library/react";
+import { getByLabelText, render } from "@testing-library/react";
 import ShopItem from "../components/ShopItem";
 
 describe("ShopItem component", () => {
     it("renders the Add To Cart button", ()=> {
         const { getByRole } = render(<ShopItem></ShopItem>);
         expect(getByRole("button").className).toMatch("AddToCartBtn");
-    })
-    it("renders the quantity input", ()=> {
-        const { getByRole } = render(<ShopItem></ShopItem>);
-        expect(getByRole("input").className).toMatch("QuantityInput");
     })
 })
