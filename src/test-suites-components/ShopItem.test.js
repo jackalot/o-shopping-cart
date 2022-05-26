784b2d3a97279c11a45f4be7573ceb8af97d92e5
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-26 08:04:55 -0400

    added imports to ShopItem.test.js

---
+++ src/test-suites-components/ShopItem.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { ... } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import userEvent from "@testing-library/user-event"; 
+import ShopItem from "../components/ShopItem";
