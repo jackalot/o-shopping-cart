6909bac6f7c5aa9dfd41c04875a5c24e318c510a
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-26 09:59:58 -0400

    CartNotification now recieves storedItems as a prop, storedItems now is empty when initialized

--- src/Shop.js
+++ src/Shop.js
@@ -3,11 +3,7 @@ import CartNotification from "./components/CartNotification";
 import ShopItem from "./components/ShopItem";
 import Cart from "./components/Cart";
 function Shop () {
-    const [storedItems, setStoredItems] = useState([{
-        name: "name",
-        price: 10.00,
-        quantity: 10,
-    }])
+    const [storedItems, setStoredItems] = useState([])
     function storeThisItem(itemName, itemPrice, itemQuantity) {
         setStoredItems([
             ...storedItems,
@@ -21,7 +17,7 @@ function Shop () {
     return (
     <div> 
         <h1> Welcome to the shop!</h1>
-        <CartNotification amount={1}></CartNotification>
+        <CartNotification amount={storedItems.length}></CartNotification>
         <Cart></Cart>
         <ShopItem handler={storeThisItem} itemName="Taco" itemPrice="10.00"></ShopItem>
     </div>
