d86b83e438f7094cd7246d7da17d13eb036c8a4b
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-27 10:47:51 -0400

    created a test

--- src/test-suites-components/CartNotification.test.js
+++ src/test-suites-components/CartNotification.test.js
@@ -3,3 +3,8 @@ import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
 import ShopItem from "../components/ShopItem";
+import CartNotification from "../components/CartNotification";
+
+describe("CartNotification Component", () => {
+    
+})
