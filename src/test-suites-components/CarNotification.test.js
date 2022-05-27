72e424f1dfa3c2c8cccfb31d07d22c0bbb92968b
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-27 10:42:31 -0400

    added imports to CarNotication.test.js

---
+++ src/test-suites-components/CarNotification.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "@testing-library/jest-dom"
+import userEvent from "@testing-library/user-event"; 
+import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
+import ShopItem from "../components/ShopItem";
