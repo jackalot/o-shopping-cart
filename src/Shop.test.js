a6330a0d117bbd251f109446bed6057a164e0e9a
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 08:07:36 -0400

    added a Shop Component

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -3,3 +3,7 @@ import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
 import Shop from "./Shop";
+
+describe("Shop Component", () => {
+    
+})
