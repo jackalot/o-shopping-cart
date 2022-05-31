63d895d69681e47fd140a2cb82739eec17e40532
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 09:43:14 -0400

    removed import of CartNotification

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -3,7 +3,6 @@ import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
 import Shop from "./Shop";
-import CartNotification from "./CartNotification";
 describe("Shop Component", () => {
     it("'Welcome to the shop!' is rendered regardless of what state Shop is in", ()=> {
         const { getByRole } = render(<Shop></Shop>);
