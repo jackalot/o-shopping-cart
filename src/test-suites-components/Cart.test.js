c153b19dbbbe9bd80f24729b2dc359bb521a3799
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 10:51:23 -0400

    created the Cart component describe section

--- src/test-suites-components/Cart.test.js
+++ src/test-suites-components/Cart.test.js
@@ -4,3 +4,7 @@ import userEvent from "@testing-library/user-event";
 import { fireEvent, getByDisplayValue, getByLabelText, getByRole, getByTestId, getByText, render } from "@testing-library/react";
 import ShopItem from "../components/ShopItem";
 import CartNotification from "../components/CartNotification";
+
+describe("Cart component", () => {
+    
+})
