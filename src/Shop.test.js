deba954726163c9cd15be80482f94c152095f820
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 08:56:03 -0400

    imported CartNotifcation to Shop.test.js, made a mock of handleOpenCart

--- src/Shop.test.js
+++ src/Shop.test.js
@@ -3,13 +3,15 @@ import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
 import Shop from "./Shop";
-
+import CartNotification from "./CartNotification";
 describe("Shop Component", () => {
     it("'Welcome to the shop!' is rendered regardless of what state Shop is in", ()=> {
         const { getByRole } = render(<Shop></Shop>);
         expect(getByRole("heading").textContent).toMatch("Welcome to the shop!");
     })
     it("Cart Notification calls handleOpenCart when clicked", () => {
-
+        function handleOpenCart(){
+            
+        }
     })
 })
