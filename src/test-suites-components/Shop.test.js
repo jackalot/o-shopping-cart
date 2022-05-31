c19d0900316704935259ff7b6d8d6988706330bb
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 10:47:51 -0400

    moved Shop.test.js to test-suits-components folder, updated imports

--- src/test-suites-components/Shop.test.js
+++ src/test-suites-components/Shop.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event"; 
 import { getByDisplayValue, getByLabelText, getByRole, render } from "@testing-library/react";
-import Shop from "./Shop";
+import Shop from "../Shop";
 describe("Shop Component", () => {
     it("'Welcome to the shop!' is rendered regardless of what state Shop is in", ()=> {
         const { getByRole } = render(<Shop></Shop>);
