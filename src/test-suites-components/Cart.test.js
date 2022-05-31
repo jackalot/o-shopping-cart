ce0e78f61ffc3a5ab2540b1fb8975760aca61e8f
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-31 10:48:50 -0400

    added new file Cart.test.js, added imports for Cart.test.js

---
+++ src/test-suites-components/Cart.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "@testing-library/jest-dom"
+import userEvent from "@testing-library/user-event"; 
+import { fireEvent, getByDisplayValue, getByLabelText, getByRole, getByTestId, getByText, render } from "@testing-library/react";
+import ShopItem from "../components/ShopItem";
+import CartNotification from "../components/CartNotification";
