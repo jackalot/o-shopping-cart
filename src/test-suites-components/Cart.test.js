15b85d1724fd9ea0df2a0c2f5e170e2a9d484d43
jackalotUVM <gonzalezc9999@gmail.com>
2022-06-01 08:22:59 -0400

    added an import to Cart, added a getByRole for the Cart Component

--- src/test-suites-components/Cart.test.js
+++ src/test-suites-components/Cart.test.js
@@ -4,9 +4,11 @@ import userEvent from "@testing-library/user-event";
 import { fireEvent, getByDisplayValue, getByLabelText, getByRole, getByTestId, getByText, render } from "@testing-library/react";
 import ShopItem from "../components/ShopItem";
 import CartNotification from "../components/CartNotification";
-
+import Cart from "../components/Cart";
 describe("Cart component", () => {
-    it("The Cart component has a 'Checkout' button")
+    it("The Cart component has a 'Checkout' button", () => {
+        const { getByRole } = render(<Cart></Cart>)
+    })
     it("The cart component has a total price element")
     it("The Cart component displays all of the needed list elements with their prices")
 })
